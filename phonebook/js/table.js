88884bd363ed2eecee6dc01b5f16e1320386dfed
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-03-25 21:52:35 +0100

    phonebook

---
+++ phonebook/js/table.js
@@ -0,0 +1,7 @@
+function phoneBook (){
+    let ime = document.getElementById ('ime').value;
+    let prezime = document.getElementById ('prezime').value;
+    let telefon = document.getElementById (telefon).value;
+    document.getElementById ('table');
+    document.getElementById ('body').innerHTML = <tr><td>${ime}</td><td>${prezime}</td><td>${telefon}</td></tr>;
+}
