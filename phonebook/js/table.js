c29c10a56895ce9a7a4cbef505167a3db0fcb7de
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-03-26 17:35:59 +0100

    correction

--- phonebook/js/table.js
+++ phonebook/js/table.js
@@ -1,7 +1,7 @@
 function phoneBook (){
     let ime = document.getElementById ('ime').value;
     let prezime = document.getElementById ('prezime').value;
-    let telefon = document.getElementById (telefon).value;
-    document.getElementById ('table');
-    document.getElementById ('body').innerHTML = <tr><td>${ime}</td><td>${prezime}</td><td>${telefon}</td></tr>;
+    let telefon = document.getElementById ('telefon').value;
+    document.getElementById ('table').style = 'display: initial' ;
+    document.getElementById ('body').innerHTML += '<tr><td>${ime}</td><td>${prezime}</td><td>${telefon}</td></tr>';
 }
